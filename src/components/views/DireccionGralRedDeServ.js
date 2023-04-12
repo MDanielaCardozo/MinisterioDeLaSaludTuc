442d75d5467c1afb41fc779bb05a121f53f06d74
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-04-12 12:18:43 -0300

    Agregué la Direccion Gral de Red de Servicios

--- src/components/views/DireccionGralRedDeServ.js
+++ src/components/views/DireccionGralRedDeServ.js
@@ -1,11 +1,109 @@
-import React from 'react';
+import React from "react";
 
 const DireccionGralRedDeServ = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="bgColorEnf">
+      <section className="container">
+        <h1>Dirección General de Red de Servicios</h1>
+        <p>
+          Esta Dirección engloba a los servicios de salud del I y II Nivel de
+          Atención distribuidos por toda la provincia. Ellos son los
+          responsables de la salud de poblaciones bien definidas, en base a
+          áreas de responsabilidad. Estas unidades de salud (CAPS, CIC, Postas
+          Sanitarias, Policlínicas/ Hospitales del II Nivel) buscan dar una
+          respuesta adecuada con la estrategia de Atención Primaria de la Salud
+          (APS), con énfasis en la accesibilidad y la continuidad de la
+          atención.
+          <br />
+          El I Nivel (CAPS, CIC, Postas Sanitarias) brinda una atención
+          ambulatoria integral con énfasis en la Prevención y Promoción de
+          salud. Estas unidades son periféricas a un II Nivel
+          (Policlínicas/Hospitales del II Nivel) que resuelven patologías de
+          mediano riesgo. Todas ellas brindan, además de la atención clínica,
+          soporte para el diagnóstico y tratamiento (Laboratorios de análisis
+          clínicos, y Radiología).
+          <br />
+          En el año 2015 se logró que el 100% de los servicios de las Áreas
+          programáticas estén informatizados.
+          <br />
+          El 100% de los Establecimientos de Salud cuenta con Farmacia, luz,
+          agua, electricidad en general, y recolección de residuos patológicos.
+        </p>
+        <ul>
+          <li>
+            <strong>
+              Directora general de la dirección general de la Red de servicio
+            </strong>{" "}
+            - Dra. Yolanda Brepe
+          </li>
+          <li>
+            <strong>Subdirector</strong> - Dr. Marcelo Montoya
+          </li>
+          <li>
+            <strong>Directora área programática Centro</strong> - Dra. Teresa
+            Mena
+          </li>
+          <li>
+            <strong>Subdirectora</strong> - Dra. Raquel Núñez Mrad
+          </li>
+          <li>
+            <strong>Directora área programática Oeste</strong> - Dra. Claudia
+            Toblli
+          </li>
+          <li>
+            <strong>Subdirector</strong> - Dr. Juan Zelarayán
+          </li>
+          <li>
+            <strong>Directora área programática Este</strong> - Dra. Alejandra
+            Trejo
+          </li>
+          <li>
+            <strong>Subdirectora</strong> - Dra. Thelma Iñigo
+          </li>
+          <li>
+            <strong>Directora área programática Sur</strong> - Dra. Marcela
+            Figueroa
+          </li>
+          <li>
+            <strong>Subdirector</strong> - Dr. Pedro Otonelo
+          </li>
+          <li>
+            <strong>Depende departamento de Recursos Técnicos</strong> -
+            Licenciado José Juárez
+          </li>
+          <li>
+            <strong>
+              Jefe de Personal de la Dirección General de Red de Servicios
+            </strong>{" "}
+            - Sr Javier Cartes.
+          </li>
+          <li>
+            <strong>
+              Administradora del departamento de Gerencia Administrativa
+            </strong>{" "}
+            - María Teresa Sued
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="http://msptucuman.gov.ar/centros-sanitarios/centros-sanitarios-consulta-grafica/">
+              Centros Sanitarios - Consulta Gráfica
+            </a>
+          </li>
+          <li>
+            <a href="http://msptucuman.gov.ar/efectores/efectores.php">
+              Listado de Efectores - Buscado
+            </a>
+          </li>
+          <li>
+            <a href="http://msptucuman.gov.ar/centros-sanitarios/centros-sanitarios-establecimientos-de-salud-en-areas-programaticas/">
+              Mapas - Áreas operativas
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default DireccionGralRedDeServ;
